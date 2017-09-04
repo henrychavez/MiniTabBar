4e0793322822dbd925ab23a6f38c13f02b05ab5c
Henry Chávez <henry19.chavez@gmail.com>
2017-09-04 14:48:07 -0500

    Fixed text position

--- MiniTabBar/Classes/MiniTabBarItemView.swift
+++ MiniTabBar/Classes/MiniTabBarItemView.swift
@@ -95,8 +95,8 @@ class MiniTabBarItemView: UIView {
             TITLE
             */
             UIView.animate(withDuration: 0.2, delay: 0, options: UIViewAnimationOptions(), animations: {
-                self.titleLabel.frame.size.height = 12
                 self.titleLabel.font = UIFont.systemFont(ofSize: 12)
+                self.titleLabel.frame.size.height = 12
             })
         }
     }
@@ -117,8 +117,8 @@ class MiniTabBarItemView: UIView {
              TITLE
              */
             UIView.animate(withDuration: 0.2, delay: 0, options: UIViewAnimationOptions(), animations: {
-                self.titleLabel.frame.size.height = 14
                 self.titleLabel.font = UIFont.systemFont(ofSize: 14)
+                self.titleLabel.frame.size.height = 14
             })
         }
     }
